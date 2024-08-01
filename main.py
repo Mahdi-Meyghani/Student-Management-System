ed3fe54a69d3629978c8aad9dc2fda16ee126b44
Mahdi <mahdimeyghani02@gmail.com>
2024-08-01 13:12:37 +0330

    Create main window with menu bar

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-from PyQt6.QtWidgets import QWidget, QApplication, QPushButton, QLineEdit, QComboBox, QLabel
+from PyQt6.QtWidgets import QWidget, QApplication, QPushButton, QLineEdit, QComboBox, QLabel, QMainWindow, QTableWidget
+from PyQt6.QtGui import QAction
 import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management System")
+
+        file_menu_bar = self.menuBar().addMenu("&File")
+        help_menu_bar = self.menuBar().addMenu("&Help")
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec())
